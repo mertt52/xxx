8e83cf5bcb3d1150c34823a91f2b3297a67a53a2
mertt52 <85098191+mertt52@users.noreply.github.com>
2021-05-31 18:55:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-$nick=$_GET['nick'];
 $session="47906786420%3A8bRnA1MXS1uHkg%3A3"; //buraya cookieeditor'den aldıgınız sessionid'i yazın.
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://www.instagram.com/'.$nick.'/?__a=1'); ////This may differ from site to site
+curl_setopt($ch, CURLOPT_URL, 'https://www.instagram.com/mert/?__a=1'); ////This may differ from site to site
 //curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 'Accept-Language: en-US,en;q=0.9',
 'Cookie: sessionid='.$session.'',
-'Referer: https://www.instagram.com/'.$nick.'/',
+'Referer: https://www.instagram.com/mert',
 'sec-fetch-dest: document',
 'sec-fetch-mode: navigate',
 'sec-fetch-site: same-origin',
